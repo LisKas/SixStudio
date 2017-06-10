14dea75931e94856eb69685165507cb20c4154b8
Alice Kasyanova <liskas87@mail.ru>
2017-06-10 14:09:45 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+                $('.carousel').slick({
+                    speed: 8000,
+                    autoplay: true,
+                    infinite: true,
+                    slidesToShow: 1,
+                    dots: false,
+                    arrows: false
+
+                });
+              });
